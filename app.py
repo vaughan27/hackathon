8f9dac4ba9a747a85faa0721d921e04b962dfc6b
Vaughan D'souza <73072657+vaughan27@users.noreply.github.com>
2023-03-23 15:08:43 +0530

    va

--- app.py
+++ app.py
@@ -1,28 +1,16 @@
-import json
+from flask import Flask, render_template, request
 import requests
-from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/A')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-
-
-@app.route('/jobs')
-def jobs():
-    publisher_id = 'c4f7f56214793037f6c572f6e3c021e88def5715a91d20e4fdac28fd1859da3f'
-    api_key = 'K07oceIgQVcOyutXP8qhROGsF8tijSdRUVWvzpKyRGYP6NNHHNF18Voa7u17tmlZ'
-    search_params = {
-        'publisher': publisher_id,
-        'q': 'python',
-        'l': 'San Francisco',
-        'jt': 'fulltime',
-        'limit': 10,
-        'format': 'json',
-        'v': '2'
-    }
-    response = requests.get('http://api.indeed.com/ads/apisearch', params=search_params)
-    jobs = response.json()['results']
-    return render_template('jobs.html', jobs=jobs)
+@app.route('/search')
+def search():
+    query = request.args.get('query')
+    url = f'https://jobs.github.com/positions.json?description={query}'
+    response = requests.get(url)
+    jobs = response.json()
+    return render_template('search.html',  jobs=jobs)
